583f890fb23520c0941a6b5414857d9d628e032d
Maksimqa322 <142583277+Maksimqa322@users.noreply.github.com>
2025-03-13 13:02:39 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,23 @@ import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  // Загрузите переменные окружения
   dotenv.config();
-  await app.listen(process.env.PORT ?? 3001);
+
+  // Создайте приложение NestJS
+  const app = await NestFactory.create(AppModule);
+
+  // Включите CORS для всех доменов
+  app.enableCors({
+    origin: '*', // Разрешить запросы со всех доменов
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Разрешенные методы
+    credentials: false, // Отключить передачу кук и заголовков авторизации
+  });
+
+  // Запустите приложение
+  const port = process.env.PORT || 3001;
+  await app.listen(port);
+  console.log(`Application is running on port ${port}`);
 }
+
 bootstrap();
