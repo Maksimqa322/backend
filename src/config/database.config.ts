a44c1bf88b377c2e26b36cf1b6fc5718b821921b
Maksimqa322 <142583277+Maksimqa322@users.noreply.github.com>
2025-03-16 20:04:47 +0300

    Update database.config.ts

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,14 +1,12 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { DataSource } from 'typeorm';
 
-export const databaseConfig = {
+export const databaseConfig = (): TypeOrmModuleOptions => ({
   type: 'postgres',
-  host: process.env.DB_HOST || '192.168.1.65',
-  port: parseInt(process.env.DB_PORT || '5432', 10),
-  username: process.env.DB_USER || 'user1',
-  password: process.env.DB_PASSWORD || 'user1',
-  database: process.env.DB_NAME || 'db',
+  host: process.env.DB_HOST, // Обязательная переменная (без дефолта)
+  port: parseInt(process.env.DB_PORT, 10),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   autoLoadEntities: true,
-  synchronize: true,
-} as TypeOrmModuleOptions;
+  synchronize: process.env.NODE_ENV !== 'production', // Отключаем для прода
+});
