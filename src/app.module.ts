f934ecbedd841659dc60cf5144311fe13516938f
Maksimqa322 <142583277+Maksimqa322@users.noreply.github.com>
2025-03-16 20:34:54 +0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,15 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { databaseConfig } from './config/database.config';
-import { MessagesController } from './modules/messages/messages.controller';
-import { MessagesService } from './modules/messages/messages.service';
 import { MessagesModule } from './modules/messages/messages.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRoot(databaseConfig),
+    ConfigModule.forRoot({ 
+      isGlobal: true,
+      envFilePath: '.env', // Добавляем явное указание на .env-файл
+    }),
+    TypeOrmModule.forRoot(databaseConfig()), // Вызываем функцию конфигурации
     MessagesModule,
   ],
   controllers: [],
