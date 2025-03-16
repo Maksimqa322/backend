7b32fe1cf5f33cd42e2ccf90549559b3fd5947f6
Maksimqa322 <142583277+Maksimqa322@users.noreply.github.com>
2025-03-16 20:35:09 +0300

    Update database.config.ts

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -3,10 +3,11 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 export const databaseConfig = (): TypeOrmModuleOptions => ({
   type: 'postgres',
   host: process.env.DB_HOST || 'database', // Значение по умолчанию для Docker
-  port: parseInt(process.env.DB_PORT || '5432', 10),
+  port: parseInt(process.env.DB_PORT || '5432', 10), // Гарантированная строка
   username: process.env.DB_USER || 'user1',
-  password: process.env.DB_USER || 'user1',
+  password: process.env.DB_PASSWORD || 'user1', // Исправлено с DB_PASSWORD
   database: process.env.DB_NAME || 'db',
   autoLoadEntities: true,
   synchronize: process.env.NODE_ENV !== 'production',
+  logging: process.env.NODE_ENV === 'development', // Логи только для разработки
 });
